96aa818147886fb592ee6250ff295eb86fb551d2
xela200121 <hhalessio@gmail.com>
2022-02-24 10:50:39 +0100

    prova 1

---
+++ src/test/java/com/expriviaproject/stage/expriviaproject/ExpriviaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expriviaproject.stage.expriviaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpriviaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
